063a9a70d9c2b79c38c1dc5eb8ef5f40b7944317
GalwayK <galwayk@sheridancollege.ca>
2022-01-25 12:56:30 -0500

    Added a new variable address and associated getter and setter method.

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,12 +2,24 @@ package demo;
 
 public class Demo 
 {
+    private String address = "991";
 
+    public String getAddress() 
+    {
+        return address;
+    }
+    
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+    
     public static void main(String[] args) 
     {
         System.out.println("Demo One.");
         System.out.println("New changes.");
         System.out.println("Even more numbers.");
+        
     }
     
 }
